e37baa13706feb831dfc13c738dd13ae9fb21f07
Carson Miller <carsondm@gmail.com>
2017-05-06 03:04:46 -0400

    Added log file for time
    After running, the program outputs the elapsed time to a file. This is
    to keep track of how long it takes over time as more processing is done

--- vc.py
+++ vc.py
@@ -1,13 +1,14 @@
 # Read a video file frame by frame, counting all occurrences of colors
 
 import numpy as np
-
+import time
 import cv2
 
 cap = cv2.VideoCapture('dance.webm')
 w = cap.get(3)  # get width, cap attribute 3
 h = cap.get(4)  # get height, cap attribute 4
 frames = 0
+start = time.time()
 
 bucket = np.zeros((256, 256, 256))
 
@@ -21,7 +22,6 @@ while(ret):
 
     # Print the top left pixel color channels (RGB)
     px = frame[0, 0]
-    # print( px )
 
     # Add the colors to the bucket
     bucket[px[0]][px[1]][px[2]] += 1
@@ -29,10 +29,20 @@ while(ret):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+# For time elapse calculations
+end = time.time()
 
+# Some probably useless output
 print("Width: ", w)
 print("Height: ", h)
 print("Frames: ", frames)
+print("Process Time: ", end - start)
+
+# Append the elapsed time to timeLog.txt
+with open("timeLog.txt", "a") as f:
+    s = str( end - start )
+    f.write( s )
+    f.write( '\n')
 
 # When everything done, release the capture
 cap.release()
